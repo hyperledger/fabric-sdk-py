c6fbd0feea0fd30481b110432718354518e383b2
Baohua Yang <baohyang@cn.ibm.com>
2017-02-10 17:21:41 +0800

    Create the framework for msp module
    This is for https://jira.hyperledger.org/browse/FAB-2160.
    
    Change-Id: Ia34f6769141279fd56494db184658fff398ef806
    Signed-off-by: Baohua Yang <baohyang@cn.ibm.com>

--- hfc/api/msp/msp.py
+++ hfc/api/msp/msp.py
@@ -5,10 +5,28 @@ class MSP(object):
     algorithms (e.g., ECDSA, RSA) and PKIs (software-managed or HSM based)
     """
 
-    def __init__(self, config):
+    def __init__(self, trusted_certs, signer, admins, crypto_suite, id):
         """ Init with configuration info.
 
         Args:
-            config: include trusted_certs, signer, admins, id, crypto_suites.
+            trusted_certs: trust anchors at boot
+            signer: signing identity
+            admins: admin privileges
+            crypto_suites: crypto algorithm family
+            id: id of the instance
         """
-        pass
+        self.trusted_certs = trusted_certs
+        self.signer = signer
+        self.admins = admins
+        self.crypto_suite = crypto_suite
+        self.id = id
+
+    def validate(self, id):
+        """ check whether the id is valid
+
+        Args:
+            id: id to check
+
+        Returns: Boolean
+        """
+        return True
