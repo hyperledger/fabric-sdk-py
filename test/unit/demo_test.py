b25069300cf344b564ef778fffdceb650729cb2f
Baohua Yang <baohyang@cn.ibm.com>
2017-02-17 10:09:22 +0800

    Fix the demo test class
    Fix the name, and add descriptions to help understand how to use.
    
    Change-Id: I29ee9ab924ef4604f3f3283b9ac468ee8bc73d57
    Signed-off-by: Baohua Yang <baohyang@cn.ibm.com>

--- test/unit/demo_test.py
+++ test/unit/demo_test.py
@@ -5,16 +5,37 @@ def add_one(x):
     return x + 1
 
 
-class ChaincodeTest(unittest.TestCase):
-    """ Test chaincode deploy, invoke and query
+class DemoTest(unittest.TestCase):
+    """ Demo a simple test case.
     """
     def setUp(self):
+        """ setUP will be called before each test method.
+
+        Returns:
+
+        """
         self.name = b'Hello world!'
 
     def tearDown(self):
+        """ tearDown will be called after each test method.
+
+        Usually can add some clean up work here.
+
+        Returns:
+
+        """
         pass
 
     def test_add_one(self):
+        """ The test method to call.
+
+        Here can call the external methods we want to test against.
+
+        Assert failure will cause exception to break the testing.
+
+        Returns:
+
+        """
         self.assertEqual(add_one(3), 4)
 
 
