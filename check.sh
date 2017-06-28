3656b0cbd8219d7558fe34178deef2a1577396f5
qiang0723 <wqiang0723@gmail.com>
2017-06-28 16:44:52 +0800

    [FAB-5027] Enable start fabric network with 1.0.0-rc1
    1.Modify images tag in check.sh
    2.Update crypto-config and channel-artifacts
    3.Update docker-compose-file according to official e2e_cli
    
    Change-Id: Ic7ca864e64edcb13ac2b36e880fe6054b179c562
    Signed-off-by: qiang0723 <wqiang0723@gmail.com>

--- check.sh
+++ check.sh
@@ -9,18 +9,25 @@ docker-compose -v
 pip install tox
 
 # pull fabric images
+ARCH=x86_64
+BASEIMAGE_RELEASE=0.3.1
+BASE_VERSION=1.0.0
+PROJECT_VERSION=1.0.0-rc1
 IMG_TAG=v1.0.0-rc1
+
 echo "===Pulling fabric images... with tag = ${IMG_TAG}"
 docker pull yeasy/hyperledger-fabric-base:$IMG_TAG
 docker pull yeasy/hyperledger-fabric-peer:$IMG_TAG
 docker pull yeasy/hyperledger-fabric-orderer:$IMG_TAG
 docker pull yeasy/hyperledger-fabric-ca:$IMG_TAG
 
-docker tag yeasy/hyperledger-fabric-base:$IMG_TAG hyperledger/fabric-baseimage
-docker tag yeasy/hyperledger-fabric-base:$IMG_TAG hyperledger/fabric-ccenv:x86_64-1.0.0-rc1
 docker tag yeasy/hyperledger-fabric-peer:$IMG_TAG hyperledger/fabric-peer
+docker tag yeasy/hyperledger-fabric-peer:$IMG_TAG hyperledger/fabric-tools
 docker tag yeasy/hyperledger-fabric-orderer:$IMG_TAG hyperledger/fabric-orderer
 docker tag yeasy/hyperledger-fabric-ca:$IMG_TAG hyperledger/fabric-ca
+docker tag yeasy/hyperledger-fabric-base:$IMG_TAG hyperledger/fabric-ccenv:$ARCH-$PROJECT_VERSION
+docker tag yeasy/hyperledger-fabric-base:$IMG_TAG hyperledger/fabric-baseos:$ARCH-$BASEIMAGE_RELEASE
+docker tag yeasy/hyperledger-fabric-base:$IMG_TAG hyperledger/fabric-baseimage:$ARCH-$BASEIMAGE_RELEASE
 
 # run tests
 echo "===Starting test..."
