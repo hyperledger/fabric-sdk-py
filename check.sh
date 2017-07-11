193723f82bd35c5d04d379db2f641968e2f6ce3d
dpdornseifer <david.paul.dornseifer@sap.com>
2017-07-11 15:53:46 -0700

    [FAB-5281] Updated to 1.0 Final
    Changed images to 1.0 Final
    
    Change-Id: Id864c1ccf882958199b711627433d96ddb92afab
    Signed-off-by: dpdornseifer <david.paul.dornseifer@sap.com>

--- check.sh
+++ check.sh
@@ -12,22 +12,22 @@ pip install tox
 ARCH=x86_64
 BASEIMAGE_RELEASE=0.3.1
 BASE_VERSION=1.0.0
-PROJECT_VERSION=1.0.0-rc1
-IMG_TAG=v1.0.0-rc1
+PROJECT_VERSION=1.0.0
+IMG_TAG=1.0.0
 
 echo "===Pulling fabric images... with tag = ${IMG_TAG}"
-docker pull yeasy/hyperledger-fabric-base:$IMG_TAG
-docker pull yeasy/hyperledger-fabric-peer:$IMG_TAG
-docker pull yeasy/hyperledger-fabric-orderer:$IMG_TAG
-docker pull yeasy/hyperledger-fabric-ca:$IMG_TAG
+docker pull hyperledger/fabric-peer:$ARCH-$IMG_TAG
+docker pull hyperledger/fabric-orderer:$ARCH-$IMG_TAG
+docker pull hyperledger/fabric-ca:$ARCH-$IMG_TAG
+docker pull hyperledger/fabric-ccenv:$ARCH-$IMG_TAG
+docker pull hyperledger/fabric-baseimage:$ARCH-$BASEIMAGE_RELEASE
+docker pull hyperledger/fabric-baseos:$ARCH-$BASEIMAGE_RELEASE
 
-docker tag yeasy/hyperledger-fabric-peer:$IMG_TAG hyperledger/fabric-peer
-docker tag yeasy/hyperledger-fabric-peer:$IMG_TAG hyperledger/fabric-tools
-docker tag yeasy/hyperledger-fabric-orderer:$IMG_TAG hyperledger/fabric-orderer
-docker tag yeasy/hyperledger-fabric-ca:$IMG_TAG hyperledger/fabric-ca
-docker tag yeasy/hyperledger-fabric-base:$IMG_TAG hyperledger/fabric-ccenv:$ARCH-$PROJECT_VERSION
-docker tag yeasy/hyperledger-fabric-base:$IMG_TAG hyperledger/fabric-baseos:$ARCH-$BASEIMAGE_RELEASE
-docker tag yeasy/hyperledger-fabric-base:$IMG_TAG hyperledger/fabric-baseimage:$ARCH-$BASEIMAGE_RELEASE
+echo "===Re-tagging images to *latest* tag"
+docker tag hyperledger/fabric-peer:$ARCH-$IMG_TAG hyperledger/fabric-peer
+docker tag hyperledger/fabric-peer:$ARCH-$IMG_TAG hyperledger/fabric-tools
+docker tag hyperledger/fabric-orderer:$ARCH-$IMG_TAG hyperledger/fabric-orderer
+docker tag hyperledger/fabric-ca:$ARCH-$IMG_TAG hyperledger/fabric-ca
 
 # run tests
 echo "===Starting test..."
