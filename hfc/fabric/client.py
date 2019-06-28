0bc1f5652a9a7dc74a20f8335827b899d1f63fb1
Kelvin Moutet <kelvin.moutet@owkin.com>
2019-06-28 09:44:42 +0200

    [FABP-244] Add docstring for set tls.
    Change-Id: I0c1b4aecad85724140d3c0233d38a072f1ec630a
    Signed-off-by: Kelvin Moutet <kelvin.moutet@owkin.com>

--- hfc/fabric/client.py
+++ hfc/fabric/client.py
@@ -280,6 +280,18 @@ class Client(object):
     def set_tls_client_cert_and_key(self, client_key_file=None,
                                     client_cert_file=None):
 
+        """Set tls client for mutual tls for all peers and orderers
+
+        Args:
+            client_key (str): file path for Private key used for TLS when
+                making client connections
+            client_cert (str): file path for X.509 certificate used for TLS
+                when making client connections
+
+        Returns:
+            bool: set success value
+        """
+
         self._client_key_path = client_key_file
         self._client_cert_path = client_cert_file
 
