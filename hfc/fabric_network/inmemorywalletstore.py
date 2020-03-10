662ef37fbf23ad7e4f9af3827e784a2134cb8202
rohanshrothrium <shrothriumrohan@gmail.com>
2020-03-11 02:49:54 +0530

    checks and comments
    Signed-off-by: rohanshrothrium <shrothriumrohan@gmail.com>

--- hfc/fabric_network/inmemorywalletstore.py
+++ hfc/fabric_network/inmemorywalletstore.py
@@ -1,20 +1,25 @@
+from hfc.fabric_ca.caservice import Enrollment
+
+
 class InMemoryWalletStore(object):
     """ InMemoryWalletStore stores the identities of users and admins
         in memory
     """
     def __init__(self):
         self._Map = {}
-    
+
     def exists(self, enrollment_id):
         """ Returns whether or not the creds of a user with a given enrollment_id
             exists in the wallet
         """
         return enrollment_id in self._Map
-        
+
     def remove(self, enrollment_id):
         """ Deletes identities of users with the given user_id """
         del self._Map[enrollment_id]
 
     def put(self, enrollment_id, user_enrollment):
         """ Saves the particular Identity in the wallet """
+        if not isinstance(user_enrollment, Enrollment):
+            raise ValueError('"user_enrollment" is not a valid Enrollment object')
         self._Map[enrollment_id] = user_enrollment
