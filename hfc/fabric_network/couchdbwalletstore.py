f6bd0467e60c204807cfc44579861e1e274d5098
rohanshrothrium <shrothriumrohan@gmail.com>
2020-03-22 23:31:41 +0530

    couch db put
    Signed-off-by: rohanshrothrium <shrothriumrohan@gmail.com>

--- hfc/fabric_network/couchdbwalletstore.py
+++ hfc/fabric_network/couchdbwalletstore.py
@@ -1,5 +1,7 @@
 import couchdb
 
+from hfc.fabric_ca.caservice import Enrollment
+
 class CouchDBWalletStore(object):
     def __init__(self, config='http://localhost:5984',  dbName):
         self.server = couchdb.Server(config)
@@ -9,4 +11,7 @@ class CouchDBWalletStore(object):
             self.db = self.server.create(dbname)
 
     def exists(self, enrollment_id):
-        
+        try:
+            enrollment_dict = db[enrollment_id]
+            if not isinstance(user, Enrollment):
+                raise ValueError('"user" is not a valid Enrollment object')
