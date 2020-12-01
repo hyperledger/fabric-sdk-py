bb944a46d90f4a6d4af5c44c70a0fa7713ed6cc9
Rajeev Ranjan <rajeev2.ranjan@intel.com>
2020-12-01 08:34:16 +0530

    Minor typo fix in contract.py
    Signed-off-by: Rajeev Ranjan <rajeev2.ranjan@intel.com>

--- hfc/fabric_network/contract.py
+++ hfc/fabric_network/contract.py
@@ -38,7 +38,7 @@ class Contract(object):
         evaluated on the list of peers discovered and then submitted to the ordering service
         for committing to the ledger.
         """
-        channel_name = self.network.channel._name
+        channel_name = self.network.channel.name
         cli = self.gatetway.client
         peers = cli._peers
 
@@ -60,7 +60,7 @@ class Contract(object):
         the ordering service and hence will not be committed to the ledger.
         This is used for querying the world state.
         """
-        channel_name = self.network.channel3
+        channel_name = self.network.channel.name
         cli = self.gatetway.client
         peers = cli._peers
 
